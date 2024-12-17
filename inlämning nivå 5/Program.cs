8b17068021368ce756d31df715cf68bd6a0f2e64
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-17 13:52:27 +0100

    Add project files.

---
+++ inlämning nivå 5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Jämna tal mellan 1 och 20:");
+
+       
+        for (int i = 2; i <= 20; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
